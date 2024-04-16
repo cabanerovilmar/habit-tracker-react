f7c2f2c005dacbc6c6eaeae3635d089d3606f1a4
Vilmar Cabañero <vilmarcabanero@gmail.com>
2024-04-16 15:29:17 +0800

    EN-1: Implement theming with dark mode using MUI

--- src/core/data/LocalStorageManager.ts
+++
@@ -1,7 +0,0 @@
-export function setDarkMode(isDarkMode: boolean) {
-  localStorage.setItem('isDarkMode', JSON.stringify(isDarkMode))
-}
-
-export function getDarkMode() {
-  return JSON.parse(localStorage.getItem('isDarkMode') || 'false') as boolean
-}
