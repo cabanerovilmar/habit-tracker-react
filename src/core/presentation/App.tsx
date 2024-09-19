5a0c0118e369a5cbefdec582cee7c3c0e71bee64
Vilmar Cabañero <vilmarcabanero@gmail.com>
2024-09-19 10:52:00 +0800

    Update dependencies to latest version except eslint.

--- src/core/presentation/App.tsx
+++ src/core/presentation/App.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from 'react'
 import { Box, Text, useMantineColorScheme } from '@mantine/core'
-import { Card } from './components/Card'
 import { useUpdatePwaThemeColor } from './theme/useUpdatePwaThemeColor'
 
 export function App() {
@@ -18,11 +17,9 @@ function CenteredMessage() {
 
   return (
     <Box className="flex justify-center items-center h-screen text-center p-4">
-      <Card className="max-w-3xl">
-        <Text className="text-2xl md:text-4xl cursor-pointer p-2" onClick={() => toggleColorScheme()}>
-          This website is under development.
-        </Text>
-      </Card>
+      <Text className="text-2xl md:text-4xl cursor-pointer p-2" onClick={() => toggleColorScheme()}>
+        This website is under development.
+      </Text>
     </Box>
   )
 }
